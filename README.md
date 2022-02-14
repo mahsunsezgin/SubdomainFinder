782919ad1b8688a13f311e081e18a1c46f8ba233
Mahsun Sezgin <80767294+mahsunsezgin@users.noreply.github.com>
2022-02-14 21:04:00 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Subdomain Finder
+>Python dili requests kütüphanesi ve subdomains.txt dosyasındaki subdomainler kullanılarak hedef domain'in subdomainlerini bulan bir araçtır.
+
+
+## Kullanılan Teknolojiler
+- Requests kütüphanesi
+## Gereksinimler
+- Python 3.9.10 sürümü
---
+++ SubdomainFinder.py
@@ -0,0 +1,18 @@
+from xml import dom
+import requests
+
+domain = input("Enter domain: ")
+
+file = open('subdomains.txt')
+content = file.read()
+subdomains = content.splitlines()
+
+for subdomain in subdomains:
+	url = "http://{}.{}".format(subdomain, domain)
+	try:
+		requests.get(url)
+		print(url)
+	except:
+		pass
+
+
